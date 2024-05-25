e3206b11bae0dd3847a2a1d719c2c16b9a1fec41
Felipe Lira <santosfelipeliipe@gmail.com>
2024-05-25 15:41:21 +0000

    feat: adição da verificação da nota e do envio da nota para mudar de página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,57 @@
 import { CardContainer, IconContainer, ButtonContainer, ResultContainer } from "./Home.styles";
 import iconStarimg from "../assets/icon-star.svg";
 import thankYouImg from "../assets/illustration-thank-you.svg";
+import { useState } from "react";
 
 export function Home() {
-    let aparecerResultado = true
+    const [mostrarResultado, setMostrarResultado] = useState(false)
+    const [feedbackNote, setfeedBackNote] = useState(0)
 
-  return (
-    aparecerResultado === false ? (
-        <CardContainer>
-            <IconContainer>
-                <img src={iconStarimg} alt="icone de estrela" />
-            </IconContainer>
+    function handleFeedbackButtonClick(event) {
+        const feedback = Number(event.target.innerText)
 
-            <h1>Como foi o atendimento?</h1>
-            <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!
-            </p>
+        setfeedBackNote(feedback)
+    }
 
-            <ButtonContainer>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-            </ButtonContainer>
+    function handleSubmit() {
+        if (feedbackNote === 0) return 
 
-            <button>Enviar</button>
-        </CardContainer>
-    ) : (
-        <CardContainer>
-            <img src={thankYouImg} alt="imagem de agradecimento" />
+        setMostrarResultado(true)
+    }
 
-            <ResultContainer>
-                <p>Você selecionou 4 de 5</p>
-            </ResultContainer>
+    return (
+        mostrarResultado === false ? (
+            <CardContainer>
+                <IconContainer>
+                    <img src={iconStarimg} alt="icone de estrela" />
+                </IconContainer>
 
-            <h1>Obrigado!</h1>
-            <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato! </p>
-        </CardContainer>
+                <h1>Como foi o atendimento?</h1>
+                <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!
+                </p>
+
+                <ButtonContainer>
+                    <button onClick={handleFeedbackButtonClick}>1</button>
+                    <button onClick={handleFeedbackButtonClick}>2</button>
+                    <button onClick={handleFeedbackButtonClick}>3</button>
+                    <button onClick={handleFeedbackButtonClick}>4</button>
+                    <button onClick={handleFeedbackButtonClick}>5</button>
+                </ButtonContainer>
+
+                <button onClick={handleSubmit}>Enviar</button>
+            </CardContainer>
+        ) : 
+        (
+            <CardContainer>
+                <img src={thankYouImg} alt="imagem de agradecimento" />
+
+                <ResultContainer>
+                    <p>Você selecionou {feedbackNote} de 5</p>
+                </ResultContainer>
+
+                <h1>Obrigado!</h1>
+                <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato! </p>
+            </CardContainer>
+        )
     )
-  )
 }
